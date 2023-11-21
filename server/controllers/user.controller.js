b87205695a5d7857658804c45f14957e7f6768b1
roypromax <roy19rishabh@gmail.com>
2023-11-22 02:16:40 +0530

    Update user signup controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,11 +6,13 @@ const userSignupController = async (req, res) => {
   try {
     const ifUserExist = await UserModel.findOne({ email });
     if (ifUserExist) {
-      res.status(200).json({ message: "User already exists" });
+      res
+        .status(200)
+        .json({ message: "User already exists", user: ifUserExist });
     } else {
       const user = new UserModel({ email, username });
       await user.save();
-      res.status(201).json({ message: "User created" });
+      res.status(201).json({ message: "User created", user });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
